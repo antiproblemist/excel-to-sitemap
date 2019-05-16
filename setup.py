236488e6e851d75393635f93c41f8e96ee41ab66
Shahzeb Qureshi <shahzeb_iam@hotmail.co.in>
2019-05-16 13:21:20 +0530

    Added setup.py for build

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+ 
+ 
+version = '0.1.0'
+
+setup(
+    name = "xl2sitemap",
+    packages = ["xl2sitemap"],
+    entry_points = {
+        "console_scripts": ['xl2sitemap = xl2sitemap.xl2sitemap:main']
+        },
+    version = version,
+    description = "Generate heavy sitemap files using excel sheets",
+    long_description = "Generate sitemap files with excel sheet as the input",
+    author = "Shahzeb Qureshi",
+    author_email = "shahzeb_iam@outlook.com",
+    url = "https://github.com/antiproblemist/xl2sitemap",
+    package_data={
+        '': ['LICENSE'],
+    },
+    install_requires=['pandas>=0.24.2', 'tqdm>=4.32.1', 'tqdm>=4.32.1', 'numpy>=1.16.3', 'xlrd>=1.2.0'
+],
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development',
+        ],
+    )
