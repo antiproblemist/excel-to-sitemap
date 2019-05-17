262f976b72413112fac3c5a166a5a4eb404f0d88
Shahzeb Qureshi <shahzeb_iam@hotmail.co.in>
2019-05-17 10:26:50 +0530

    chnages in description

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
 from setuptools import setup
-  
-version = '0.1.1'
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+version = '0.1.2'
 
 setup(
     name = "xl2sitemap",
@@ -10,16 +16,15 @@ setup(
         },
     version = version,
     description = "Generate heavy sitemap files using excel sheets",
-    long_description = "Generate sitemap files with excel sheet as the input",
+    long_description = long_description,
     author = "Shahzeb Qureshi",
     author_email = "shahzeb_iam@outlook.com",
-    url = "https://github.com/antiproblemist/xl2sitemap",
+    url = "https://github.com/antiproblemist/excel-to-sitemap",
     include_package_data = True,
     package_data={
-        '': ['LICENSE'],
+        '': ['LICENSE', 'README.md'],
     },
-    install_requires=['pandas>=0.24.2', 'tqdm>=4.32.1', 'tqdm>=4.32.1', 'numpy>=1.16.3', 'xlrd>=1.2.0'
-],
+    install_requires=['pandas>=0.24.2', 'tqdm>=4.32.1', 'numpy>=1.16.3', 'xlrd>=1.2.0'],
     classifiers=[
         'Environment :: Console',
         'Intended Audience :: Developers',
