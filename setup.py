7106d96face8f315b6f4e9eedb0373509ea818b6
Shahzeb Qureshi <shahzeb_iam@hotmail.co.in>
2019-05-18 09:10:30 +0530

    removed encoding in open to support previous versions

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from os import path
 
 this_directory = path.abspath(path.dirname(__file__))
 
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+with open(path.join(this_directory, 'README.md')) as f:
     long_description = f.read()
 
-version = '0.1.2'
+version = '0.1.3'
 
 setup(
     name = "xl2sitemap",
